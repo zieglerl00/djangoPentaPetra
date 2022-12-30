cf2979fbb7f8357f793d6218f0763aae924bc7c5
zieglerl00 <lukas.ziegler52@gmail.com>
2022-12-30 20:50:16 +0100

    update settings

--- djangoPentaPetra/settings.py
+++ djangoPentaPetra/settings.py
@@ -15,10 +15,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    "*"
+    "*",
+    "65.109.161.27"
 ]
 
 
