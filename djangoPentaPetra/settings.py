793a875f6116ce5072b11de59841a1ca69f14cc6
zieglerl00 <lukas.ziegler52@gmail.com>
2022-12-30 21:34:53 +0100

    update settings

--- djangoPentaPetra/settings.py
+++ djangoPentaPetra/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*",
@@ -115,6 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / "pentapetra/static"
 
 STATIC_URL = '/static/'
 
--- djangoPentaPetra/urls.py
+++ djangoPentaPetra/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = "djangoPentaPetra"
+
 urlpatterns = [
     path("auth/", include("authentication.urls")),
     path("", include("pentapetra.urls")),
