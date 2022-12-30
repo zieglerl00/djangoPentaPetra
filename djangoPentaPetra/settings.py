e61dff970c1ba72d57b4d971190573f2e9e66307
zieglerl00 <lukas.ziegler52@gmail.com>
2022-12-30 21:38:37 +0100

    update settings

--- djangoPentaPetra/settings.py
+++ djangoPentaPetra/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "pentapetra/static"
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
