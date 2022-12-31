82b5fe52218e5977658882dd511bf791f1b898f5
zieglerl00 <lukas.ziegler52@gmail.com>
2022-12-31 13:59:33 +0100

    update settings

--- djangoPentaPetra/settings.py
+++ djangoPentaPetra/settings.py
@@ -14,6 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+CORS_ALLOWED_ORIGINS = [
+    "https://dashboard.penta-petra.at/"
+]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
